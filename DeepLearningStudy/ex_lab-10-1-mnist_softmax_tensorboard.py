dfd0f25b31e8ef9aa60b6bde5ba36de6734f1eea
NAM-IL <newlaser@hanmail.net>
2017-07-01 06:53:11 +0900

    test

---
+++ DeepLearningStudy/ex_lab-10-1-mnist_softmax_tensorboard.py
@@ -0,0 +1,118 @@
+# Lab 7 Learning rate and Evaluation
+import os
+# silence INFO logs set it to 1
+# filter out WARNING set it to 2 
+# silence ERROR logs (not recommended) set it to 3
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+import random
+import matplotlib.pyplot as plt
+from tensorflow.examples.tutorials.mnist import input_data
+
+tf.set_random_seed(777)  # reproducibility
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# Check out https://www.tensorflow.org/get_started/mnist/beginners for
+# more information about the mnist dataset
+
+# parameters
+learning_rate = 0.01
+training_epochs = 15
+batch_size = 100
+
+
+# input place holders
+X = tf.placeholder(tf.float32, [None, 784])
+Y = tf.placeholder(tf.float32, [None, 10])
+
+with tf.variable_scope('layer1') as scope:
+    # weights & bias for nn layers
+    W = tf.Variable(tf.random_normal([784, 10]))
+    b = tf.Variable(tf.random_normal([10]))
+    
+    hypothesis = tf.matmul(X, W) + b
+    
+    x_image = tf.reshape(X, [-1, 28, 28, 1])
+    
+    tf.summary.image('input', x_image, 3)
+    w1_hist = tf.summary.histogram("weights", W)
+    b1_hist = tf.summary.histogram("biases", b)
+    hypothesis_hist = tf.summary.histogram("hypothesis", hypothesis)
+
+
+# define cost/loss & optimizer
+with tf.name_scope("cost") as scope:
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+        logits=hypothesis, labels=Y))
+   
+    cost_summ = tf.summary.scalar("cost", cost)
+   
+with tf.name_scope("train") as scope:
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+
+    
+# initialize
+sess = tf.Session()
+
+merged_summary = tf.summary.merge_all()
+writer = tf.summary.FileWriter("./logs/mnist_softmax_0_01")
+writer.add_graph(sess.graph)  # Show the graph
+
+sess.run(tf.global_variables_initializer())
+
+global_step = 0
+
+# train my model
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys}
+        summary, c, _ = sess.run([merged_summary, cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+        writer.add_summary(summary, global_step=global_step)
+        global_step += 1
+        
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+print('Learning Finished!')
+
+# Test model and check accuracy
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: mnist.test.images, Y: mnist.test.labels}))
+
+# Get one and predict
+r = random.randint(0, mnist.test.num_examples - 1)
+print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+print("Prediction: ", sess.run(tf.argmax(hypothesis, 1), 
+                               feed_dict={X: mnist.test.images[r:r + 1]}))
+
+plt.imshow(mnist.test.images[r:r + 1].reshape(28, 28), 
+           cmap='Greys', interpolation='nearest')
+plt.show()
+
+'''
+Epoch: 0001 cost = 5.888845987
+Epoch: 0002 cost = 1.860620173
+Epoch: 0003 cost = 1.159035648
+Epoch: 0004 cost = 0.892340870
+Epoch: 0005 cost = 0.751155428
+Epoch: 0006 cost = 0.662484806
+Epoch: 0007 cost = 0.601544010
+Epoch: 0008 cost = 0.556526115
+Epoch: 0009 cost = 0.521186961
+Epoch: 0010 cost = 0.493068354
+Epoch: 0011 cost = 0.469686249
+Epoch: 0012 cost = 0.449967254
+Epoch: 0013 cost = 0.433519321
+Epoch: 0014 cost = 0.419000337
+Epoch: 0015 cost = 0.406490815
+Learning Finished!
+Accuracy: 0.9035
+'''
