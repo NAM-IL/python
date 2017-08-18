f02e0240ba299d8ae444d410917c6146e4598c2b
NAM-IL <newlaser@hanmail.net>
2017-08-19 04:51:22 +0900

    test

--- DeepLearningStudy/Decision_Trees/Decision_Tree_IG.py
+++ DeepLearningStudy/Decision_Trees/Decision_Tree_IG.py
@@ -7,9 +7,9 @@ SCIKIT-LEARN : DECISION TREE LEARNING II - CONSTRUCTING THE DECISION TREE
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+from matplotlib.colors import ListedColormap
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
@@ -17,9 +17,7 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-tree = DecisionTreeClassifier(criterion='entropy',
-                       max_depth=3, random_state=0)
-
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
 tree.fit(X_train, y_train)
 
 X_combined = np.vstack((X_train, X_test))
@@ -34,10 +32,13 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
    # plot the decision surface
    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+   
    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-   np.arange(x2_min, x2_max, resolution))
+                          np.arange(x2_min, x2_max, resolution))
+   
    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
    Z = Z.reshape(xx1.shape)
+   
    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
    plt.xlim(xx1.min(), xx1.max())
    plt.ylim(xx2.min(), xx2.max())
@@ -48,6 +49,7 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
       plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
                alpha=0.8, c=cmap(idx),
                marker=markers[idx], label=cl)
+      
    # highlight test samples
    if test_idx:
       X_test, y_test = X[test_idx, :], y[test_idx]
@@ -55,8 +57,8 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                alpha=1.0, linewidth=1, marker='o',
                s=55, label='test set')
 
-plot_decision_regions(X_combined, y_combined,
-         classifier=tree, test_idx=range(105,150))
+plot_decision_regions(X_combined, y_combined, classifier=tree, 
+                      test_idx=range(105,150))
 
 plt.xlabel('petal length [cm]')
 plt.ylabel('petal width [cm]')
