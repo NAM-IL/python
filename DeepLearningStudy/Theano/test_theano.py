75e5e7180ee191af27bacfd024bc679e601c043d
NAM-IL <newlaser@hanmail.net>
2017-09-15 16:10:24 +0900

    test

---
+++ DeepLearningStudy/Theano/test_theano.py
@@ -0,0 +1,45 @@
+import theano
+from theano import tensor as T
+
+ 
+
+x = T.vector('x')
+W = T.matrix('W')
+b = T.vector('b')
+
+dot = T.dot(x, W)
+out = T.nnet.sigmoid(dot + b)
+
+from theano.printing import debugprint
+debugprint(dot)
+
+debugprint(out)
+
+f = theano.function(inputs=[x, W], outputs=dot)
+g = theano.function([x, W, b], out)
+h = theano.function([x, W, b], [dot, out])
+i = theano.function([x, W, b], [dot + b, out])
+
+debugprint(f)
+
+debugprint(g)
+
+ 
+
+
+from theano.printing import pydotprint 
+import pydot
+import graphviz
+import pydot_ng as pydot
+
+pydotprint(f, outfile='pydotprint_f.png')
+
+# from IPython.display import Image
+# Image('pydotprint_f.png', width=1000)
+# 
+# 
+# pydotprint(g, outfile='pydotprint_g.png')
+# Image('pydotprint_g.png', width=1000)
+
+
+
